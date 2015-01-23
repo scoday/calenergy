c6532f131f943a86e127b9deeb57e4167ca2254d
Scott Day <sday@mac.com>
2015-01-23 17:54:41 +0000

    Also updated for bsd style things and stuff.

--- bin/postprocessing/shell/php_graph.sh
+++ bin/postprocessing/shell/php_graph.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 #
-cd /srv/www/htdocs/php
+cd /usr/local/www/apache24/data/calenergy/htdocs/php
 mydir=$PWD
 
-YESTERDAY=`date --date yesterday +%Y-%m-%d`
-#YESTERDAY=`date --date yesterday +%Y-%m-%d`
-#YESTERDAY=`date --date "2 days ago" +%Y-%m-%d`
-PREVIOUS_DAY=`date --date "2 days ago" +%Y-%m-%d`
+YESTERDAY=`date -v -1d +%Y-%m-%d`
+PREVIOUS_DAY=`date -v -1d +%Y-%m-%d`
 
 replace() {
-    for i in hydro.php imports.php nuclear.php renewable.php smallhydro.php solarpv_production.php solarthermal.php thermal.php wind.php ; do sed -i s/$PREVIOUS_DAY/$YESTERDAY/g $i; done 
+    for i in hydro.php imports.php nuclear.php renewable.php smallhydro.php solarpv_production.php solarthermal.php thermal.php wind.php ; do sed -i "" "s/$PREVIOUS_DAY/$YESTERDAY/g" $i; done 
 }
 
 dryrun() {
