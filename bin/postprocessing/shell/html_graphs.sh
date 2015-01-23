c74533e5f4e663fa8e74bc8f0c475cf648b0d251
Scott Day <sday@mac.com>
2015-01-23 17:48:22 +0000

    Fixed this up for different platform.

--- bin/postprocessing/shell/html_graphs.sh
+++ bin/postprocessing/shell/html_graphs.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 #
-
-cd /srv/www/htdocs/generation
+# Updating web DirectoryHome
+cd /usr/local/www/apache24/data/calenergy/htdocs/generation
 mydir=$PWD
+# Adjusted date for FreeBSD #
+YESTERDAY=`date -v -1d +%m-%d-%Y`
+PREVIOUS_DAY=`date -v -2d +%m-%d-%Y`
 
-YESTERDAY=`date --date yesterday +%m-%d-%Y`
-#YESTERDAY=`date --date "3 days ago" +%m-%d-%Y`
-PREVIOUS_DAY=`date --date "2 days ago" +%m-%d-%Y`
-
+# Note the sed change, big difference between linux and bsd methods.
 replace() {
-    for i in hydro.html imports.html nuclear.html renewable.html smallhydro.html solarpv.html solarthermal.html thermal.html wind.html ; do sed -i s/$PREVIOUS_DAY/$YESTERDAY/g $i; done
+    for i in hydro.html imports.html nuclear.html renewable.html smallhydro.html solarpv.html solarthermal.html thermal.html wind.html ; do sed -i "" "s/$PREVIOUS_DAY/$YESTERDAY/g" $i; done
 }
 
 replace
